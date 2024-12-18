dd0d8dfbc8c6d733bfafe55bf003061e4b1597ce
k1lls3x <118112928+k1lls3x@users.noreply.github.com>
2024-12-18 16:43:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <cassert>
+#include <cassert> 
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -14,13 +14,100 @@ path operator""_p(const char* data, std::size_t sz) {
     return path(data, data + sz);
 }
 
-// напишите эту функцию
-bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories);
+bool Preprocess(std::ifstream& input, std::ofstream& output,
+                const path& current_file, const vector<path>& include_directories) {
+    const std::regex include_global(R"(\s*#\s*include\s*<([^>]*)>\s*)");
+    const std::regex include_local(R"(\s*#\s*include\s*"([^"]*)\"\s*)");
+
+    std::string line;
+    int line_count = 0;
+
+    while (std::getline(input, line)) {
+        ++line_count;
+
+        std::smatch match;
+        if (std::regex_match(line, match, include_local)) {
+      
+            std::filesystem::path included_file = match[1].str();
+            std::filesystem::path included_path = current_file.parent_path() / included_file;
+
+            std::ifstream included_input(included_path);
+            if (!included_input.is_open()) {
+         
+                bool found = false;
+                for (const auto& dir : include_directories) {
+                    included_path = dir / included_file;
+                    included_input.open(included_path);
+                    if (included_input.is_open()) {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    std::cout << "unknown include file " << included_file.string()
+                              << " at file " << current_file.string()
+                              << " at line " << line_count << "\n";
+                    return false;
+                }
+            }
+
+            if (!Preprocess(included_input, output, included_path, include_directories)) {
+                return false;
+            }
+            included_input.close();
+
+        } else if (std::regex_match(line, match, include_global)) {
+   
+            std::filesystem::path included_file = match[1].str();
+            std::ifstream included_input;
+            std::filesystem::path included_path;
+            bool found = false;
+
+            for (const auto& dir : include_directories) {
+                included_path = dir / included_file;
+                included_input.open(included_path);
+                if (included_input.is_open()) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                std::cout << "unknown include file " << included_file.string()
+                          << " at file " << current_file.string()
+                          << " at line " << line_count << "\n";
+                return false;
+            }
+
+            if (!Preprocess(included_input, output, included_path, include_directories)) {
+                return false;
+            }
+            included_input.close();
+
+        } else {
+
+            output << line << '\n';
+        }
+    }
+
+    return true;
+}
+
+bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories) {
+    std::ifstream input(in_file);
+    if(!input) {
+        return false;
+    }
+    std::ofstream output(out_file);
+    if(!output) {
+        return false;
+    }
+    return Preprocess(input, output, in_file, include_directories);
+}
 
 string GetFileContents(string file) {
     ifstream stream(file);
-
-    // конструируем string по двум итераторам
     return {(istreambuf_iterator<char>(stream)), istreambuf_iterator<char>()};
 }
 
@@ -71,7 +158,7 @@ void Test() {
     }
 
     assert((!Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p,
-                                  {"sources"_p / "include1"_p,"sources"_p / "include2"_p})));
+                        {"sources"_p / "include1"_p,"sources"_p / "include2"_p})));
 
     ostringstream test_out;
     test_out << "// this comment before include\n"
